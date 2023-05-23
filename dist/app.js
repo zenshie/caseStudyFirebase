9646b4a5071de65e1df148f53bad45099230ef9f
shiezen.tabanao <shiezen.tabanao@gmail.com>
2023-05-23 12:51:23 +0800

    update changes

--- dist/app.js
+++ dist/app.js
@@ -9,13 +9,12 @@ import { getFirestore, collection, addDoc, getDocs  } from "https://www.gstatic.
  // Your web app's Firebase configuration
  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
  const firebaseConfig = {
- apiKey: "AIzaSyCzf803qRjCMmK9wGhzPs68WLUZKr-M42M",
- authDomain: "casestudy-1c77a.firebaseapp.com",
- projectId: "casestudy-1c77a",
- storageBucket: "casestudy-1c77a.appspot.com",
- messagingSenderId: "743404711750",
- appId: "1:743404711750:web:fffdbd353973f645397f47",
- measurementId: "G-YX6H49NVFB"
+  apiKey: "AIzaSyCv1ThMfxRucVuyynkACX5uq6_ButWXHTE",
+  authDomain: "case-study-v2.firebaseapp.com",
+  projectId: "case-study-v2",
+  storageBucket: "case-study-v2.appspot.com",
+  messagingSenderId: "507346444713",
+  appId: "1:507346444713:web:036c62bd76da635a9ae4b8"
 };
  
  // Initialize Firebase
